5081bf7d43c03e2a744155b4c5c4a364bc976ab6
kkeejjuunn <kejun0529@gmail.com>
2019-08-28 21:18:50 +0800

    Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,7 +14,8 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        ArrayList<String> lists = new ArrayList<String>();
+        //ArrayList<String> lists = new ArrayList<String>();
+        ArrayList<Task> lists = new ArrayList<Task>();
 
         System.out.println("___________________________________________");
 
@@ -28,16 +29,29 @@ public class Duke {
 
         while(!command.equals("bye")){
             if(!command.equals("list")){
-                lists.add(command);
-                System.out.println("___________________________________________");
-                System.out.println("added: " + command);
-                System.out.println("___________________________________________");
+                if (command.substring(0, 4).equals("done")){
+                    String[] specificCommand = command.split(" ");
+                    int index = Integer.parseInt(specificCommand[1]) - 1;
+                    Task t = lists.get(index);
+                    t.isDone = true;
+                    System.out.println("___________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  [" + lists.get(index).isDone + "] " + lists.get(index).description);
+                    System.out.println("___________________________________________");
+                }
+                else{
+                    lists.add(new Task(command));
+                    System.out.println("___________________________________________");
+                    System.out.println("added: " + command);
+                    System.out.println("___________________________________________");
+                }
             }
             else{
                 if(lists.size() > 0){
                     System.out.println("___________________________________________");
+                    System.out.println("Here are the tasks in your list:");
                     for(int i = 0; i < lists.size(); i++) {
-                        System.out.println((i+1) + ". " + lists.get(i));
+                        System.out.println((i+1) + ".[" + lists.get(i).getStatusIcon() + "] " + lists.get(i).description);
                     }
                     System.out.println("___________________________________________");
                 }
