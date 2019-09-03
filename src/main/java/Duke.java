f85654f75c4d49c59f4c31cc378f47af1f919cc2
kkeejjuunn <kejun0529@gmail.com>
2019-09-04 01:38:29 +0800

    add: data/duke.txt, edit: src/main/java/Duke.java, src/main/java/ReadFile.java, src/main/java/WriteFile.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,7 +7,9 @@ public class Duke {
      *
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String file_name = "data/duke.txt";
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -22,6 +25,54 @@ public class Duke {
         System.out.println("Hello! I am Duke\nWhat can I do do for you?");
 
         System.out.println("___________________________________________");
+
+        //read file from duke.txt
+        try{
+            ReadFile file = new ReadFile(file_name);
+            String[] aryLines = file.OpenFile();
+
+            for (int i=0; i<aryLines.length;i++){
+                String individualTask = aryLines[i];
+                String[] taskToBeRead = individualTask.split(" \\| ");
+                switch (taskToBeRead[0]){
+                    case "T":
+                        lists.add(new Todo(taskToBeRead[2]));
+                        if (taskToBeRead[1] == "1"){
+                            Task t = lists.get(i);
+                            t.isDone = true;
+                            System.out.println("[T][\u2713] " + taskToBeRead[2]);
+                        }
+                        else{
+                            System.out.println("[T][\u2718] " + taskToBeRead[2]);
+                        }
+                        break;
+                    case "D":
+                        lists.add(new Deadline(taskToBeRead[2],taskToBeRead[3]));
+                        if (taskToBeRead[1] == "1"){
+                            Task t = lists.get(i);
+                            t.isDone = true;
+                            System.out.println("[D][\u2713] " + taskToBeRead[2] + " (by: " + taskToBeRead[3] + ")");
+                        }
+                        else{
+                            System.out.println("[D][\u2718] " + taskToBeRead[2] + " (by: " + taskToBeRead[3] + ")");
+                        }
+                        break;
+                    case "E":
+                        lists.add(new Event(taskToBeRead[2],taskToBeRead[3]));
+                        if (taskToBeRead[1] == "1"){
+                            Task t = lists.get(i);
+                            t.isDone = true;
+                            System.out.println("[E][\u2713] " + taskToBeRead[2] + " (at: " + taskToBeRead[3] + ")");
+                        }
+                        else{
+                            System.out.println("[E][\u2718] " + taskToBeRead[2] + " (at: " + taskToBeRead[3] + ")");
+                        }
+                        break;
+                }
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
 
         Scanner input = new Scanner(System.in);
 
@@ -50,36 +101,50 @@ public class Duke {
                         }
                     }
                     else{
-                        if (command.startsWith("todo")){
-                            if (command.length() < 6){
-                                try{
-                                    throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
-                                }catch(DukeException e){
-                                    e.printStackTrace();
+                        try{
+                            WriteFile data = new WriteFile(file_name, true);
+                            String taskToBeWritten;
+
+                            if (command.startsWith("todo")){
+                                if (command.length() < 6){
+                                    try{
+                                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                                    }catch(DukeException e){
+                                        e.printStackTrace();
+                                    }
+                                }
+                                else{
+                                    command = command.substring(5);
+                                    lists.add(new Todo(command));
+                                    System.out.println("Got it. I've added this task:");
+                                    System.out.println("  [T][" + lists.get(lists.size()-1).getStatusIcon() + "] " + lists.get(lists.size()-1).description);
+                                    taskToBeWritten = "T | 0 | " + command;
+                                    data.writeToFile(taskToBeWritten);
                                 }
                             }
-                            else{
-                                command = command.substring(5);
-                                lists.add(new Todo(command));
+                            else if (command.startsWith("deadline")){
+                                command = command.substring(9);
+                                String[] deadlineCommand = command.split(" /by ");
+                                lists.add(new Deadline(deadlineCommand[0], deadlineCommand[1]));
                                 System.out.println("Got it. I've added this task:");
-                                System.out.println("  [T][" + lists.get(lists.size()-1).isDone + "] " + lists.get(lists.size()-1).description);
+                                System.out.println("  [D][" + lists.get(lists.size()-1).isDone + "] " + lists.get(lists.size()-1).description + " (by: " + deadlineCommand[1] + ")");
+                                taskToBeWritten = "D | 0 | " + deadlineCommand[0] + " | " + deadlineCommand[1];
+                                data.writeToFile(taskToBeWritten);
                             }
+                            else if (command.startsWith("event")){
+                                command = command.substring(6);
+                                String[] eventCommand = command.split(" /at");
+                                lists.add(new Event(eventCommand[0], eventCommand[1]));
+                                System.out.println("Got it. I've added this task:");
+                                System.out.println("  [E][" + lists.get(lists.size()-1).isDone + "] " + lists.get(lists.size()-1).description + " (at: " + eventCommand[1] + ")");
+                                taskToBeWritten = "E | 0 | " + eventCommand[0] + " | " + eventCommand[1];
+                                data.writeToFile(taskToBeWritten);
+                            }
+                            System.out.println("Now you have " + lists.size() + " tasks in the list.");
+
+                        }catch (IOException e) {
+                            System.out.println("Error initializing stream");
                         }
-                        else if (command.startsWith("deadline")){
-                            command = command.substring(9);
-                            String[] deadlineCommand = command.split(" /by ");
-                            lists.add(new Deadline(deadlineCommand[0], deadlineCommand[1]));
-                            System.out.println("Got it. I've added this task:");
-                            System.out.println("  [D][" + lists.get(lists.size()-1).isDone + "] " + lists.get(lists.size()-1).description + " (by: " + deadlineCommand[1] + ")");
-                        }
-                        else if (command.startsWith("event")){
-                            command = command.substring(6);
-                            String[] eventCommand = command.split(" /at");
-                            lists.add(new Event(eventCommand[0], eventCommand[1]));
-                            System.out.println("Got it. I've added this task:");
-                            System.out.println("  [E][" + lists.get(lists.size()-1).isDone + "] " + lists.get(lists.size()-1).description + " (at: " + eventCommand[1] + ")");
-                        }
-                        System.out.println("Now you have " + lists.size() + " tasks in the list.");
                     }
                     System.out.println("___________________________________________");
                 }
